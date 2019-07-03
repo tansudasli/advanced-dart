e337f296865fb2757dea521e716db6d5bb1330f0
Tansu Dasli <tansudasli@gmail.com>
2019-07-03 12:09:46 +0300

    Initialize - restructured

---
+++ core/bin/main.dart
@@ -0,0 +1,40 @@
+import 'package:core/core.dart' as core;
+
+// we are importing PROJECT_FOLDER/lib/core.dart
+
+
+main(List<String> arguments) {
+  bool isOn = true;
+
+  num age = 10;
+  int people = 5;
+  int itemCount = int.parse('12');
+  double temp = 58.06;
+  String name = 'ali veli vahap';
+
+  const String surname = 'Deli';
+  // gets compile time error !
+  //surname = 'veli'''
+
+  arguments.forEach((e) =>
+      print('Hello world: ${core.calculate()}! ${e}'));
+
+  isOn ? print('on') : print('off');
+
+  print(temp);
+  print(itemCount);
+  print(name.substring(2));
+
+  print(name.indexOf('v'));
+
+  List<String> l = name.split(new RegExp(r"\s"));
+  print('before sort(): ' + l.toString());
+  print(l..sort());  //print at same line
+  print('after sort(): ' + l.toString());
+  l.sort((a, b) => a.length - b.length);
+  print('after length sort(): ' + l.toString());
+
+
+
+}
+
